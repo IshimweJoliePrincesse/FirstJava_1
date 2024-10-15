44523ee39c59ff366385ec0a1e99d27ec528e513
IshimweJoliePrincesse <148842543+IshimweJoliePrincesse@users.noreply.github.com>
2024-10-15 09:59:40 +0200

    java

---
+++ Developer.java
@@ -0,0 +1,25 @@
+
+class Developer {
+    private String computerType;
+
+ 
+    public Developer(String computerType) {
+        this.computerType = computerType;
+    }
+
+   
+    public void runCode() {
+        System.out.println("The developer is using a " + computerType + " to run code.");
+    }
+
+    
+    public static void main(String[] args) {
+        
+        Developer john = new Developer("laptop");
+        john.runCode(); 
+        
+        Developer jane = new Developer("desktop");
+        jane.runCode();
+}
+
+}
